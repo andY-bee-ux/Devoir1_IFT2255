815b2c703fa1c5f716152e930afdbee32b59e8f5
musa223-ui <moussaasog223@gmail.com>
2025-11-23 09:03:04 -0500

    Ajout de codes dans les classes du fichiers /controller.

--- IFT2255_Implementation/src/main/java/org/example/controller/CoursController.java
+++ IFT2255_Implementation/src/main/java/org/example/controller/CoursController.java
@@ -1,4 +1,19 @@
 package org.example.controller;
 
+import org.example.model.Avis;
+import org.example.model.Cours;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoursController {
+
+    // A faire
+    public List<Avis> getAvis(Cours cours){return new ArrayList<>();}
+
+    // A faire
+    public List<String> comparerCombinaisonCours(int[] idCours) {return new ArrayList<>();}
+
+    // A faire
+    public List<String> comparerCours(int[] idCours){return new ArrayList<>();}
 }
--- IFT2255_Implementation/src/main/java/org/example/controller/UserController.java
+++ IFT2255_Implementation/src/main/java/org/example/controller/UserController.java
@@ -1,4 +1,21 @@
 package org.example.controller;
 
+import org.example.model.Avis;
+import org.example.model.Profil;
+import org.example.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserController {
+
+    // A faire
+    public Profil getProfil(int id){return new Profil();}
+
+    // A faire
+    public List<User> getAllUsers(){return new ArrayList<>();}
+
+    // A faire
+    public List<Avis> getAvis(int id){return new ArrayList<>();}
+
 }
