a9f24d1811c7433ac050d1c5080754e8ac672145
musa223-ui <moussaasog223@gmail.com>
2025-11-22 21:09:48 -0500

    Ajouts des classes de tests avec une methode vide.

---
+++ IFT2255_Implementation/src/test/java/org/example/model/ProfilTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class ProfilTest {
+    // Ne tener pas compte de cette methode, c'est juste pour avoir un truc a ajouter sur GitHub.
+    @Test
+    public void test1(){
+    }
+}
