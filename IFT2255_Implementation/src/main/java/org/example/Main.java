e9e6fc74be05796d886d11ba1918ad8c7a756fb0
Tidjani <tidjanioff@gmail.com>
2025-11-29 15:07:14 -0500

    Update Main.java

--- IFT2255_Implementation/src/main/java/org/example/Main.java
+++ IFT2255_Implementation/src/main/java/org/example/Main.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import io.javalin.Javalin;
+import org.example.controller.CoursController;
+import org.example.service.CoursService;
+
 public class Main {
+    public static void main(String[] args) {
+
+        CoursService service = CoursService.getInstance();
+        CoursController controller = new CoursController();
+
+        Javalin app = Javalin.create().start(7070);
+
+        app.get("/cours/search", ctx -> {
+            controller.handleSearchREST(ctx);
+
+            if (!service.lastSearchUsedLocal()) {
+                ctx.header("Source", "planifium");
+            } else {
+                ctx.header("Source", "local-json");
+            }
+        });
+
+        System.out.println("PickCourse démarré sur http://localhost:7070");
+    }
 }
