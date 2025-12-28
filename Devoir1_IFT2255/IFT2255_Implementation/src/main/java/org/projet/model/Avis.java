0ee4a499dbff62b96e3617cf6bad75db5cd5a2cd
andY-bee-ux <andynoukoua@yahoo.com>
2025-12-28 08:25:14 -0500

    Implement getter and setter for 'valide' field in order for « Avis logic » to work
    Added getter and setter for 'valide' field.

--- Devoir1_IFT2255/IFT2255_Implementation/src/main/java/org/projet/model/Avis.java
+++ Devoir1_IFT2255/IFT2255_Implementation/src/main/java/org/projet/model/Avis.java
@@ -1,12 +1,11 @@
+
 package org.projet.model;
 
 /**
  * Cette classe permet de définir l'entité Avis.
  */
 public class Avis {
-    //private int id;
     private String commentaire;
-    //private int idAuteur;
     private int noteDifficulte;
     private String sigleCours;
     private boolean valide;
@@ -46,6 +45,7 @@ public class Avis {
         return sigleCours;
     }
 
+
     /**
      * Cette méthode permet de récupérer le nom de professeur.
      * @return le nom d'un professeur.
@@ -79,6 +79,22 @@ public class Avis {
 
     public int getNoteChargeTravail() {
         return noteChargeTravail;
+    }
+
+    /**
+     * Cette méthode permet de get le champ valide.
+     * @return  valide.
+     */
+    public boolean isValide() {
+        return valide;
+    }
+
+    /**
+     * Cette méthode permet de set le champ valide.
+     * @param valide booleen.
+     */
+    public void setValide(boolean valide){
+this.valide = valide;
     }
 
 }
